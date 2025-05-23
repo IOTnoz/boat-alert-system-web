5bdbb68bb068bb9f4fe47e0465dcfd50146e56d7
perdidev <perdidev@gmail.com>
2025-05-24 00:13:31 +0800

    refactor: improve security issue on firebase config

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,14 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAmLINKTlVAki7GVprNiVF-m30yvKbc9jY",
-    authDomain: "boat-safe-system.firebaseapp.com",
-    databaseURL:
-        "https://boat-safe-system-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "boat-safe-system",
-    storageBucket: "boat-safe-system.firebasestorage.app",
-    messagingSenderId: "459240538181",
-    appId: "1:459240538181:web:054097b5cee3d98a13e32c",
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app =
