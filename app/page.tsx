9761a5d9b777b97b91f3fe3019a6d336167ad383
perdidev <perdidev@gmail.com>
2025-06-16 01:57:10 +0800

    update: remove tabs on the side of the map

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { Map, Table } from "lucide-react";
 import Navbar from "@/components/layout/root/navbar";
 import BigTitle from "@/components/layout/root/big-title";
 import TeamCards from "@/components/layout/root/team-card";
@@ -17,19 +15,9 @@ export default function Home() {
     return (
         <div>
             <Navbar />
-            <div className="h-[90vh] flex px-4 gap-4">
-                <div className="flex-none w-fit flex flex-col h-full justify-center gap-4">
-                    <Button variant="outline" size="icon" className="w-16 h-16">
-                        <Map className="w-12 h-12" />
-                    </Button>
-                    <Button variant="outline" size="icon" className="w-16 h-16">
-                        <Table className="w-12 h-12" />
-                    </Button>
-                </div>
-                <div className="grow">
-                    <div className="bg-blue-100 rounded-md h-full w-full overflow-hidden">
-                        <Maps />
-                    </div>
+            <div className="px-4 md:px-8">
+                <div className="h-[90vh] bg-blue-100 rounded-md w-full overflow-hidden">
+                    <Maps />
                 </div>
             </div>
             <BigTitle />
